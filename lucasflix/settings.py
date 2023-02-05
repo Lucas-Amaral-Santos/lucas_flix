cc17e9812e41f793939f594ffc8b71b09386022c
Lucas Amaral dos Santos Barroso Leite <lucas.barroso@fiocruz.br>
2023-02-05 13:41:46 -0300

    Ajustes

--- lucasflix/settings.py
+++ lucasflix/settings.py
@@ -131,7 +131,9 @@ MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
